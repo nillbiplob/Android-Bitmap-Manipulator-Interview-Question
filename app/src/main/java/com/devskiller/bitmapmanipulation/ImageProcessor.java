6a625d3c0b0a3ec92b77ea4ae4406d669645b08c
Shafiul Alam Biplob <javaorjava@gmail.com>
2022-02-06 18:01:16 +0600

    Solution

--- app/src/main/java/com/devskiller/bitmapmanipulation/ImageProcessor.java
+++ app/src/main/java/com/devskiller/bitmapmanipulation/ImageProcessor.java
@@ -1,14 +1,27 @@
 package com.devskiller.bitmapmanipulation;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.location.Location;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
 
+import com.aapbd.appbajarlib.image.BitmapUtils;
 import com.arasthel.asyncjob.AsyncJob;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -38,11 +51,11 @@ final class ImageProcessor {
 
     @SuppressWarnings("ConstantConditions")
     void processImage(
-        final @NonNull Uri streamSource,
-        final @NonNull String outputDirectoryPath,
-        final @NonNull String note,
-        final @NonNull Location location,
-        final @NonNull ProcessImageCallback callback
+            final Context mainActivity, final @NonNull Uri streamSource,
+            final @NonNull String outputDirectoryPath,
+            final @NonNull String note,
+            final @NonNull Location location,
+            final @NonNull ProcessImageCallback callback
     ) {
         AsyncJob.doInBackground(new AsyncJob.OnBackgroundJob() {
 
@@ -52,6 +65,57 @@ final class ImageProcessor {
                 final String fileName = "bitmapmanipulator_" + System.currentTimeMillis() + ".jpeg";
                 new File(outputDirectoryPath).mkdirs();
                 final String outputPath = (outputDirectoryPath + fileName);
+
+                Log.e("Output file path", outputPath);
+
+                Bitmap bitmap=getContactBitmapFromURI(mainActivity,streamSource);
+
+                Log.e("Bitmap found after", "getContactBitmapFromURI");
+
+
+                bitmap=BitmapUtils.getResizedBitmap(bitmap, mMaxDimension);
+
+                Log.e("Bitmap found after", "getResizedBitmap");
+
+
+                // inverse bitmap color
+
+                bitmap=  inverseBitmapColors(bitmap);
+
+                Log.e("Bitmap found after", "inverseBitmapColors");
+
+
+                File file= saveBitmapIntoSDCardImage(mainActivity, outputPath,bitmap);
+
+                Log.e("Bitmap File found after", "saveBitmapIntoSDCardImage");
+
+
+
+                if(file != null)
+                {
+                    try {
+                        addExtraData(outputPath,note,location);
+
+                        Log.e("Bitmap File found after", "addExtraData");
+
+
+                        callback.onSuccess(outputPath);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+                        callback.onFailure();
+                    }
+
+
+                }else
+                {
+
+                    Log.e("Bitmap File ", "Not Found");
+
+                    callback.onFailure();
+                }
+
 
                 //START CHANGES
                 //END CHANGES
@@ -75,9 +139,90 @@ final class ImageProcessor {
     ) throws IOException {
         //START CHANGES
         //END CHANGES
+
+
+        ExifInterface exif = new ExifInterface(bitmapPath);
+        exif.setAttribute("description", note);
+        exif.saveAttributes();
+
+        exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE,
+                String.valueOf(location.getLatitude()));
+
+        exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE,
+                String.valueOf(location.getLongitude()));
+
+        exif.saveAttributes();
+
+        Log.d("exif value","added");
     }
 
     private void callCallbackOnMainThread(@NonNull AsyncJob.OnMainThreadJob job) {
         AsyncJob.doOnMainThread(job);
     }
+
+    public Bitmap getContactBitmapFromURI(Context context, Uri uri) {
+        try {
+
+            InputStream input = context.getContentResolver().openInputStream(uri);
+            if (input == null) {
+                return null;
+            }
+            return BitmapFactory.decodeStream(input);
+        }
+        catch (FileNotFoundException e)
+        {
+
+        }
+        return null;
+
+    }
+
+    public  File saveBitmapIntoSDCardImage(Context context, String outputPath, Bitmap finalBitmap) {
+
+
+        File file = new File (outputPath);
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return file;
+    }
+
+
+    public Bitmap inverseBitmapColors(Bitmap src)
+    {
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+
+        ColorMatrix matrixGrayscale = new ColorMatrix();
+        matrixGrayscale.setSaturation(0);
+
+        ColorMatrix matrixInvert = new ColorMatrix();
+        matrixInvert.set(new float[]
+                {
+                        -1.0f, 0.0f, 0.0f, 0.0f, 255.0f,
+                        0.0f, -1.0f, 0.0f, 0.0f, 255.0f,
+                        0.0f, 0.0f, -1.0f, 0.0f, 255.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+                });
+        matrixInvert.preConcat(matrixGrayscale);
+
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrixInvert);
+        paint.setColorFilter(filter);
+
+        canvas.drawBitmap(src, 0, 0, paint);
+        return bitmap;
+    }
+
 }
--- app/src/main/java/com/devskiller/bitmapmanipulation/MainActivity.java
+++ app/src/main/java/com/devskiller/bitmapmanipulation/MainActivity.java
@@ -3,12 +3,16 @@ package com.devskiller.bitmapmanipulation;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -18,15 +22,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.core.app.ActivityCompat;
+
+import com.aapbd.appbajarlib.notification.BusyDialog;
 
 public final class MainActivity extends AppCompatActivity {
 
     private static final int GIVE_GALLERY_PERMISSIONS_REQUEST_CODE = 277;
     private static final int PICK_GALLERY_IMAGE_REQUEST_CODE = 9826;
-    private static final String SAMPLE_NOTE = "note";
+    private static final String SAMPLE_NOTE = "Biplob note";
+
+    // directory should be document to be saved for the EXIF value.
     private static final String OUTPUT_DIRECTORY_PATH =
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + File.separator;
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getAbsolutePath() + File.separator;
+
 
     @SuppressWarnings("FieldCanBeLocal")
     private static Location sLondonLocation;
@@ -63,13 +73,24 @@ public final class MainActivity extends AppCompatActivity {
 
     private ImageProcessor mImageProcessor;
 
+    private AppCompatImageView imageView;
+
+     BusyDialog busyDialog;
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         findViewById(R.id.activity_main_select_image_button).setOnClickListener(new OpenGalleryClickListener(this));
+
+        imageView=findViewById(R.id.activity_main_image_view);
+
         mImageProcessor = new ImageProcessor(getResources().getInteger(R.integer.image_max_dimension), getContentResolver());
+
+
     }
 
     @Override
@@ -118,16 +139,28 @@ public final class MainActivity extends AppCompatActivity {
 
     @SuppressWarnings("ConstantConditions")
     private void processImage(Intent data) {
-         shareImage(data.getData(), OUTPUT_DIRECTORY_PATH, SAMPLE_NOTE, sLondonLocation, new ProcessImageCallback() {
+
+        busyDialog=new BusyDialog(MainActivity.this, true,"Processing");
+        busyDialog.show();
+
+
+        shareImage(data.getData(), OUTPUT_DIRECTORY_PATH, SAMPLE_NOTE, sLondonLocation, new ProcessImageCallback() {
 
             @Override
             public void onSuccess() {
+
+                Log.e("shareImage", "onSuccess called");
+
+
+
+
                 showMessage(R.string.share_image_success);
             }
 
             @Override
             public void onFailure() {
                 showMessage(R.string.process_image_failure);
+
             }
          });
     }
@@ -144,21 +177,54 @@ public final class MainActivity extends AppCompatActivity {
         final @NonNull Location location,
         final @NonNull ProcessImageCallback callback
     ) {
-        mImageProcessor.processImage(imageUri, outputDirectoryPath, note, location, new ImageProcessor.ProcessImageCallback() {
+        mImageProcessor.processImage(this,imageUri, outputDirectoryPath, note, location, new ImageProcessor.ProcessImageCallback() {
+
+
 
             @Override
             public void onSuccess(@NonNull String outputPath) {
+
+                Log.e("processImage", "onSuccess called");
+
+                if(busyDialog!=null)
+                busyDialog.dismis();
+
+
+                showTheImageIntoUI(outputPath);
+
                 final Intent shareIntent = new Intent(Intent.ACTION_SEND);
                 shareIntent.setType("image/jpeg");
                 shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(outputPath));
                 startActivity(Intent.createChooser(shareIntent, getString(R.string.share_with)));
+
                 callback.onSuccess();
             }
+
 
             @Override
             public void onFailure() {
                 callback.onFailure();
+                if(busyDialog!=null)
+                    busyDialog.dismis();
+
             }
         });
+    }
+
+
+    private void showTheImageIntoUI(String outputPath) {
+
+        Log.e("showTheImageIntoUI", " path is "+outputPath);
+
+        File imgFile = new File(outputPath);
+        if(imgFile.exists())
+        {
+            Log.e("showTheImageIntoUI", " File is exists");
+
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            imageView.setImageBitmap(myBitmap);
+        }
+
+
     }
 }
